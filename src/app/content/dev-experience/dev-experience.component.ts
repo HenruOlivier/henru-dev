2a1a09ce1c5e3bf1e18e464f0e4f34eb5c2631b4
Henru Olivier <henrupetrusolivier@gmail.com>
2024-05-12 15:27:13 +0200

    styiling and filling sections with all necessary content

--- src/app/content/dev-experience/dev-experience.component.ts
+++ src/app/content/dev-experience/dev-experience.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { TypingEffectDirective } from '../../shared/directives/typingEffect';
+import { AddInViewOnceDirective } from '../../shared/directives/addInViewOnce';
+import { ToggleInViewDirective } from '../../shared/directives/toggleInViewClass';
 
 @Component({
   selector: 'app-dev-experience',
   standalone: true,
-  imports: [TypingEffectDirective],
+  imports: [TypingEffectDirective, ToggleInViewDirective],
   templateUrl: './dev-experience.component.html',
   styleUrl: './dev-experience.component.scss'
 })
--- src/app/content/technologies/technologies.component.ts
+++ src/app/content/technologies/technologies.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TypingEffectDirective } from '../../shared/directives/typingEffect';
+import { ToggleInViewDirective } from '../../shared/directives/toggleInViewClass';
 
 @Component({
   selector: 'app-technologies',
   standalone: true,
-  imports: [],
+  imports: [TypingEffectDirective, ToggleInViewDirective],
   templateUrl: './technologies.component.html',
   styleUrl: './technologies.component.scss'
 })
