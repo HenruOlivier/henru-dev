488c8ff3caca11243d744ad093176f23547b67bb
Henru Olivier <henrupetrusolivier@gmail.com>
2024-06-17 22:38:02 +0200

    added cv view

--- src/app/content/intro-page/intro-page.component.ts
+++ src/app/content/intro-page/intro-page.component.ts
@@ -23,4 +23,8 @@ export class IntroPageComponent {
         });
   }
 
+  onCvClick() {
+    window.open('/assets/HenruOlivier-CV.pdf', '_blank');
+  }
+
 }
--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -20,4 +20,8 @@ export class FooterComponent {
         });
   }
 
+  onCvClick() {
+    window.open('/assets/HenruOlivier-CV.pdf', '_blank');
+  }
+
 }
