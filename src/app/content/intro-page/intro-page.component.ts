ad33550225c77e7be13b3230c5e7da2558da023e
Henru Olivier <henrupetrusolivier@gmail.com>
2024-07-23 07:55:19 +0200

    added env files

--- src/app/content/intro-page/intro-page.component.ts
+++ src/app/content/intro-page/intro-page.component.ts
@@ -24,7 +24,7 @@ export class IntroPageComponent {
   }
 
   onCvClick() {
-    window.open('/assets/HenruOlivier-CV.pdf', '_blank');
+    window.open('assets/HenruOlivier-CV.pdf', '_blank');
   }
 
 }
