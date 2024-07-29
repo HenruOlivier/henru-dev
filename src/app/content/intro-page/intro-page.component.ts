96c7eea39ada143456de1935d0bd9f2a6118d4c4
Henru Olivier <henrupetrusolivier@gmail.com>
2024-07-29 20:33:20 +0200

    cv update, sidebar transition update, number copied indicator

--- src/app/content/intro-page/intro-page.component.ts
+++ src/app/content/intro-page/intro-page.component.ts
@@ -2,21 +2,28 @@ import { Component } from '@angular/core';
 import { AddInViewOnceDirective } from '../../shared/directives/addInViewOnce';
 import { TypingEffectDirective } from '../../shared/directives/typingEffect';
 import { ToggleInViewDirective } from '../../shared/directives/toggleInViewClass';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-intro-page',
   standalone: true,
-  imports: [AddInViewOnceDirective, TypingEffectDirective, ToggleInViewDirective],
+  imports: [CommonModule, AddInViewOnceDirective, TypingEffectDirective, ToggleInViewDirective],
   templateUrl: './intro-page.component.html',
   styleUrl: './intro-page.component.scss'
 })
 export class IntroPageComponent {
+
+  copied: boolean = false;
 
   copyNumber() {
     const phoneNumber = '+27 82 828 9723';
     navigator.clipboard.writeText(phoneNumber)
         .then(() => {
             console.log('Phone number copied to clipboard');
+            this.copied = true;
+            setTimeout(() => {
+              this.copied = false;
+            }, 3000);
         })
         .catch(err => {
             console.error('Could not copy phone number: ', err);
